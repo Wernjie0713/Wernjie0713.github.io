01d48c15d5b3b31f2221c6f33a4c5860d15ab0f7
YONG WERN JIE <129282000+Wernjie0713@users.noreply.github.com>
2026-05-10 16:19:01 +0800

    Update resume URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const RESUME_URL = 'https://drive.google.com/file/d/1cPqJVsMPIBOpIb1FLNAiMcelgSbh2sWE/view?usp=sharing';
+export const RESUME_URL = 'https://drive.google.com/file/d/179TgjuEGAg_VibyCRRunfNhBXmZ2V4ZE/view?usp=sharing';
